f4955a0e8b38066f20367ebd84b4ddffec3fb3c6
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2020-10-08 18:09:59 +0300

    Update zoo_kneighbors.py

--- zoo_kneighbors.py
+++ zoo_kneighbors.py
@@ -5,23 +5,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report, confusion_matrix
 
-df = pd.read_csv('zoo.data')
+df = pd.read_csv('zoo.data') # Read the dataset
 
-X = df.iloc[:,1:17].values
-y = np.array(df.iloc[:,17:]).reshape(len(X),)
+X = df.iloc[:,1:17].values # Get the features as input values
+y = np.array(df.iloc[:,17:]).reshape(len(X),) # Shape the output to fit the model properly
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) # Split the entire data to train and test packages.
 
 
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
+knn = KNeighborsClassifier(n_neighbors=3) # Model setup
+knn.fit(X_train, y_train) # Fit the train input and output to the model
+
 #scr = knn.score(X_test, y_test) # alternative accuracy score
 
-y_pred = knn.predict(X_test[:])
+y_pred = knn.predict(X_test[:]) # Predict whole test data
 print(y_pred)
 
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred) # Get the accuracy score
 print(accuracy)
 
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
+print(confusion_matrix(y_test,y_pred)) # Print the confusion matrix
+print(classification_report(y_test,y_pred)) # Print the model report to examine precision, recall and f-measure values
